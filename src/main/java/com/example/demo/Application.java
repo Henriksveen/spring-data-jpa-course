6ce7513b45d853c94c984268d18c1e22dfd08c03
Simen <simen.henriksveen@gmail.com>
2022-01-28 00:11:44 +0100

    add faker dependecies

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,11 +1,10 @@
 package com.example.demo;
 
+import com.github.javafaker.Faker;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import java.util.List;
 
 @SpringBootApplication
 public class Application {
@@ -17,41 +16,19 @@ public class Application {
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
         return args -> {
-            System.out.println("Adding Maria and Ahmed");
-            studentRepository.saveAll(
-                    List.of(Student.builder()
-                                    .firstName("Maria")
-                                    .lastName("Jones")
-                                    .email("maria.jones@amigoscode.edu")
-                                    .age(21).build(),
-                            Student.builder()
-                                    .firstName("Maria")
-                                    .lastName("Jones")
-                                    .email("maria2.jones@amigoscode.edu")
-                                    .age(25).build(),
-                            Student.builder()
-                                    .firstName("Ahmed")
-                                    .lastName("Ali")
-                                    .email("ahmed.ali@amigoscode.edu")
-                                    .age(18).build()
-                    )
-            );
-
-            studentRepository.findStudentByEmail("ahmed.ali@amigoscode.edu")
-                    .ifPresentOrElse(System.out::println, () -> System.out.println("Student with email not found"));
-
-
-            studentRepository.findStudentByEmail("unknown@amigoscode.edu")
-                    .ifPresentOrElse(System.out::println, () -> System.out.println("Student with email not found"));
-
-            studentRepository.findStudentsByFirstNameEqualsAndAgeIsGreaterThanEqual("Maria", 21)
-                    .forEach(System.out::println);
-
-            studentRepository.findStudentsByFirstNameEqualsAndAgeIsGreaterThanEqualNative("Maria", 21)
-                    .forEach(System.out::println);
-
-            System.out.println("Deleting Maria 2");
-            System.out.println(studentRepository.deleteStudentById(3L));
+            Faker faker = new Faker();
+            for (int i = 0; i <= 20; i++) {
+                String firstName = faker.name().firstName();
+                String lastName = faker.name().lastName();
+                String email = String.format("%s.%s@amigoscode.edu", firstName, lastName);
+                Student student = Student.builder()
+                        .firstName(firstName)
+                        .lastName(lastName)
+                        .email(email)
+                        .age(faker.number().numberBetween(17, 55))
+                        .build();
+                studentRepository.save(student);
+            }
         };
     }
 }
